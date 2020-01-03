28d72fc43d6c091223432b182d6a369dad091dd8
Mihkel Eidast <mihkel@play.ee>
2020-01-03 18:35:45 +0200

    allow handlePrefix option to be used with render tag

---
+++ src/utils.js
@@ -0,0 +1,10 @@
+module.exports = {
+    isHandle(str, handlePrefix) {
+        return str && str.startsWith(handlePrefix);
+    },
+    replaceHandlePrefix(handle, handlePrefix) {
+        let prefixMatcher = new RegExp(`^\\${handlePrefix}`);
+
+        return handle.replace(prefixMatcher, '@')
+    },
+};
